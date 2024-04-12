d9ad340468a957eaa9851d84925afacebd73823c
NiLS Lederer <nils.lederer@fau.de>
2024-04-12 17:52:15 +0800

    final

--- ansible-emmy/roles/configure-lmod/files/intelmpi-2021.12.lua
+++ ansible-emmy/roles/configure-lmod/files/intelmpi-2021.12.lua
@@ -1,4 +1,4 @@
-local base = pahtJoin("/apps/intel/oneapi/mpi/2021.12/")
+local base = pathJoin("/apps/intel/oneapi/mpi/2021.12")
 
 family("mpi")
 
@@ -6,7 +6,10 @@ whatis("Name: "..myModuleName())
 whatis("Version: "..myModuleVersion())
 whatis("Category: Development")
 
-prepend_path("PATH", pathJoin(base, "bin")
+prepend_path("PATH", pathJoin(base, "bin"))
 prepend_path("CPATH", pathJoin(base, "include"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
-setenv("INTEL_ONEAPI_MPI_ROOT", "/apps/intel/oneapi")
+prepend_path("CMAKE_PREFIX_PATH", pathJoin("/apps/intel/oneapi"))
+
+setenv("INTEL_ONEAPI_MPI_ROOT", pathJoin("/apps/intel/oneapi"))
+setenv("I_MPI_ROOT", base)
